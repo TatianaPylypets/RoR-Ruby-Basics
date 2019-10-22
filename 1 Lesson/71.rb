7e75fc441528e06d53e7837f7ef37fa4a556e840
TatianaPylypets <cbeeiy12@gmail.com>
2019-10-22 12:47:24 +0300

    hometask2/task0

--- 1 Lesson/71.rb
+++ 1 Lesson/71.rb
@@ -1,16 +1,5 @@
-#Дан целочисленный массив. Удалить все элементы, встречающиеся менее двух раз.
+# Дан целочисленный массив. Удалить все элементы, встречающиеся менее двух раз.
 a = [9, 2, 6, 3, 45, 2, 95, 905, 23, 6, 34, 6, 67, 23]
-
-puts
-puts "Дан целочисленный массив:  #{a}"
-puts
-bad_elements = a.uniq.select {|x| a.count(x) == 1}
-print "Элементы, встречающиеся менее двух раз: "
-print bad_elements
-puts
-puts
-print "Целочисленный массив после удаления элементов, встречающихся менее двух раз: "
-bad_elements.each {|x| a.delete(x)}
+bad_elements = a.uniq.select { |x| a.count(x) == 1 }
+bad_elements.each { |x| a.delete(x) }
 print a
-puts
-puts
--- 1 Lesson/73.rb
+++ 1 Lesson/73.rb
@@ -1,16 +1,5 @@
-#Дан целочисленный массив. Удалить все элементы, встречающиеся ровно два раза.
+# Дан целочисленный массив. Удалить все элементы, встречающиеся ровно два раза.
 a = [9, 2, 6, 23, 45, 2, 95, 905, 23, 6, 34, 67, 23]
-
-puts
-puts "Дан целочисленный массив:  #{a}"
-puts
-bad_elements = a.uniq.select {|x| a.count(x) == 2}
-print "Элементы, встречающиеся ровно два раза: "
-print bad_elements
-puts
-puts
-print "Целочисленный массив после удаления элементов, встречающихся ровно два раза: "
-bad_elements.each {|x| a.delete(x)}
+bad_elements = a.uniq.select { |x| a.count(x) == 2 }
+bad_elements.each { |x| a.delete(x) }
 print a
-puts
-puts
